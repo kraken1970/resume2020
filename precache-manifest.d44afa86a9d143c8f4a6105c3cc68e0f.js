b686c1ee6c0495358f72b9018d1e3555fcb039bf
kraken1970 <everestdon@ukr.net>
2020-10-15 15:19:12 +0300

    Updates

---
+++ precache-manifest.d44afa86a9d143c8f4a6105c3cc68e0f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2df6fa86826334be01fab9b438d5e0e",
+    "url": "/resume2020/index.html"
+  },
+  {
+    "revision": "50bf2fd9aa18c5cd94bd",
+    "url": "/resume2020/static/css/main.dbabc78b.chunk.css"
+  },
+  {
+    "revision": "9f0d131db1c3cadbd103",
+    "url": "/resume2020/static/js/2.702e78a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume2020/static/js/2.702e78a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bf2fd9aa18c5cd94bd",
+    "url": "/resume2020/static/js/main.4fa39482.chunk.js"
+  },
+  {
+    "revision": "087cbf435ad21efc9d24",
+    "url": "/resume2020/static/js/runtime-main.091894a7.js"
+  },
+  {
+    "revision": "d0f4ff4ed440b0fb49a3d7cb14cc26c5",
+    "url": "/resume2020/static/media/mail.d0f4ff4e.svg"
+  },
+  {
+    "revision": "9ebc997aeaabdae143e30085fd57b623",
+    "url": "/resume2020/static/media/p2.9ebc997a.jpg"
+  },
+  {
+    "revision": "7c4d15235d14bb3918e9ff1fe02e170b",
+    "url": "/resume2020/static/media/phone.7c4d1523.svg"
+  },
+  {
+    "revision": "eaf87e9dc71898185d9008f2ae3484d7",
+    "url": "/resume2020/static/media/pic01.eaf87e9d.jpg"
+  },
+  {
+    "revision": "ff6e468b412fa7328aab14b65160662c",
+    "url": "/resume2020/static/media/pic02.ff6e468b.jpg"
+  },
+  {
+    "revision": "27d5d309df8feb30e771170c7856d054",
+    "url": "/resume2020/static/media/pic03.27d5d309.jpg"
+  },
+  {
+    "revision": "dacc56d13b4bf26fc922279bdff13070",
+    "url": "/resume2020/static/media/pic04.dacc56d1.jpg"
+  },
+  {
+    "revision": "84826979ad54a6b358ce725e0f3126ef",
+    "url": "/resume2020/static/media/pic05.84826979.jpg"
+  },
+  {
+    "revision": "706d7ba38a78d418d7437942b46cdd32",
+    "url": "/resume2020/static/media/pic06.706d7ba3.jpg"
+  },
+  {
+    "revision": "01a415905fc938c16042a49251e0705b",
+    "url": "/resume2020/static/media/skype.01a41590.svg"
+  },
+  {
+    "revision": "11a0e4282e76c36c22b3a780dfa3f68e",
+    "url": "/resume2020/static/media/telegram.11a0e428.svg"
+  },
+  {
+    "revision": "93e06585287bee2b8ec783a69b578d88",
+    "url": "/resume2020/static/media/viber.93e06585.svg"
+  }
+]);
