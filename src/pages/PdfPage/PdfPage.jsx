d181744a434493a4b17b04eeadcded92038777d1
kraken1970 <everestdon@ukr.net>
2021-02-23 16:27:27 +0300

    ++ type in embed

--- src/pages/PdfPage/PdfPage.jsx
+++ src/pages/PdfPage/PdfPage.jsx
@@ -3,7 +3,7 @@ import Pdf from "assets/Resume.pdf";
 import s from "./PdfPage.module.scss";
 
 const PdfPage = () => {
-  return <embed src={Pdf} className={s.pdf} />;
+  return <embed src={Pdf} className={s.pdf} type="application/pdf" />;
 };
 
 export default PdfPage;
